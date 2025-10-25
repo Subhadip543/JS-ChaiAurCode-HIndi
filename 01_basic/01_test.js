df52b4b3c1487c840a41d880dc8edcfbf7651fc5
Subhadip543 <subhakarmakardip543@gmail.com>
2025-10-25 02:27:53 +0000

    container change

---
+++ 01_basic/01_test.js
@@ -0,0 +1,10 @@
+let username = "Subha";
+let greetMessage = `Hello ${username} !`;
+console.log(greetMessage);
+console.log(typeof(greetMessage));
+
+
+let multiplication  = `Value of multiplication is : ${2 * 2}`;
+console.log(multiplication);
+console.log(typeof(multiplication));
+
